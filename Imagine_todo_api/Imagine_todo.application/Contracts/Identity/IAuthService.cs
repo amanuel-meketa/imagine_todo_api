774deb374f6b513c1df1a77cea6ff5752371ea2b
Amanuel <Amanuel.Eshete1@gmail.com>
2024-03-15 10:20:30 +0300

    Added JWT authentication models to handle user credentials and implemented login and register contracts.

---
+++ Imagine_todo_api/Imagine_todo.application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Imagine_todo.application.Model.Identity;
+
+namespace Imagine_todo.application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+
+    }
+}
---
+++ Imagine_todo_api/Imagine_todo.application/Model/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Imagine_todo.application.Model.Identity
+{
+    public class User
+    {
+        public string? Id { get; set; }
+        public string? Email { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+    }
+}
