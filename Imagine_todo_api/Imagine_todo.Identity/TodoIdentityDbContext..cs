7c8217235c6eb0058dd40eaad672e5c8309bddd2
Amanuel <Amanuel.Eshete1@gmail.com>
2024-03-15 10:50:31 +0300

    Added a new Identity class library to manage user authentication and authorization. Included configuration files for user, role, and user-role configuration data seeder.

---
+++ Imagine_todo_api/Imagine_todo.Identity/TodoIdentityDbContext..cs
@@ -0,0 +1,24 @@
+﻿using Imagine_todo.Identity.Configurations;
+using Imagine_todo.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imagine_todo.Identity
+{
+    public class TodoIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TodoIdentityDbContext(DbContextOptions<TodoIdentityDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
